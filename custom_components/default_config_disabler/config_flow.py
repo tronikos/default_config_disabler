094f5dcb3beff57a8dcb877bfe183d64aa509e7b
tronikos <tronikos@users.noreply.github.com>
2025-01-25 18:37:25 -0800

    Avoid deprecation log warning
    Fixes https://github.com/tronikos/default_config_disabler/issues/7

--- custom_components/default_config_disabler/config_flow.py
+++ custom_components/default_config_disabler/config_flow.py
@@ -46,7 +46,6 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
 
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
         """Initialize options flow."""
-        self.config_entry = config_entry
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
